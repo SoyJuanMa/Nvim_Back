a7e62db56f0f176bd703595467103a9eb8841d6d
SoyJuanMa <juanmanuelamado100@outlook.com>
2025-07-10 11:42:40 -0300

    nvim_back

---
+++ nvim/lua/plugins/gitsings.lua
@@ -0,0 +1,30 @@
+return {
+  "lewis6991/gitsigns.nvim",
+  enabled = true,
+  opts = {
+    signs = {
+      add = { text = "" },
+      change = { text = "|" },
+      delete = { text = "-" },
+      topdelete = { text = "_" },
+      changedelete = { text = "~" },
+    },
+    current_line_blame = true,
+    current_line_blame_opts = {
+      virt_text = true,
+      virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+      delay = 0,
+      ignore_whitespace = false,
+    },
+    current_line_blame_formatter = "<author>, <author_time:%d-%m-%Y %H:%M> ",
+
+    sign_priority = 6,
+    update_debounce = 100,
+    status_formatter = nil,
+  },
+  config = function(_, opts)
+    require("gitsigns").setup(opts)
+    -- Definís el highlight después de cargar el plugin
+    vim.api.nvim_set_hl(0, "GitSignsCurrentLineBlame", { fg = "#E80C6D", bg = "none", italic = true })
+  end,
+}
---
+++ wezterm/wezterm.lua
@@ -0,0 +1,38 @@
+local wezterm = require("wezterm")
+local config = {}
+
+config.keys = {
+	{
+		key = "d",
+		mods = "ALT",
+		action = wezterm.action.SplitPane({
+			direction = "Down",
+
+			size = { Percent = 40 },
+		}),
+	},
+	{
+		key = "j",
+		mods = "ALT",
+		action = wezterm.action.ActivatePaneDirection("Up"),
+	},
+
+	{
+		key = "k",
+		mods = "ALT",
+		action = wezterm.action.ActivatePaneDirection("Down"),
+	},
+}
+
+-- Resto de tu config...
+config.color_scheme = "Kanagawa (Gogh)"
+config.window_padding = { bottom = 0, top = 0, right = 0, left = 0 }
+config.font = wezterm.font("VictorMono Nerd Font")
+config.hide_tab_bar_if_only_one_tab = true
+config.max_fps = 240
+config.enable_kitty_graphics = true
+config.window_background_opacity = 0.85
+config.font_size = 15.5
+config.enable_scroll_bar = false
+
+return config
